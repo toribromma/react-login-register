e12ec7d189467bd5e0a409b2c98d081c89c38c65
toritheterrible <torimpham@gmail.com>
2020-06-27 12:50:18 -0700

    create a Dashboard component once logged in

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
-
 class Register extends Component {
   constructor() {
     super();
