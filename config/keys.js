944e3e37c9687eb211b5d31a60f544532d967f86
toritheterrible <torimpham@gmail.com>
2020-06-27 11:05:38 -0700

    added passport for JSON web token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://toriB:GNW8mN4HUYDZUVb@cluster0-0czhn.mongodb.net/<dbname>?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://toriB:GNW8mN4HUYDZUVb@cluster0-0czhn.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    secretOrKey: "secret"
   };
 
