3c25d8ac6b55114ba5583a003e4138215cb69415
toritheterrible <torimpham@gmail.com>
2020-06-27 12:15:35 -0700

    added register and login components

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+
 class App extends Component {
   render() {
     return (
