67886de9a12a2210f55f65ac082a2b03707dfcc3
toritheterrible <torimpham@gmail.com>
2020-06-27 12:05:42 -0700

    added landing page and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+        <Navbar />
+        <Landing />
+        </Router>
+      </div>
+    );
+  }
 }
-
 export default App;
